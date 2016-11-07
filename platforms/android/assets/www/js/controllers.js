94c0c4072647fa2efe814f9a2aee0febe1d19130
humblehound <dejtabejz@gmail.com>
2016-11-07 10:55:39 +0100

    Init

---
+++ platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,165 @@
+angular.module('starter.controllers', [])
+
+  .controller('ItemController', function ($scope, $state, Item, $http, $stateParams) {
+
+    if ($stateParams.id != "") {
+      $http.defaults.headers.common['x-access-token'] = sessionStorage.getItem("token");
+      $scope.item = Item.get({id: $stateParams.id});
+      $scope.title = "Edit item";
+      $scope.action = function (item) {
+        Item.update({id: item._id}, item);
+      }
+    } else {
+      $scope.title = "New item";
+      $scope.item = {};
+      $scope.action = function (item) {
+        Item.save(item);
+      }
+    }
+
+    $scope.back = function () {
+      $state.go('itemList', {}, {reload: true});
+    };
+
+    $scope.save = function (form) {
+      if (form.$valid) {
+        var token = sessionStorage.getItem("token");
+        var userId = sessionStorage.getItem("userId");
+        $http.defaults.headers.common['x-access-token'] = token;
+        $scope.item.owner = userId;
+        $scope.action($scope.item);
+        $state.go('itemList', {}, {reload: true});
+      }
+    };
+  })
+
+  .controller('ItemListController', function ($scope, $http, $ionicPopup, $state, $stateParams, $ionicHistory, $resource, $ionicSideMenuDelegate, Notification, User, Item) {
+
+    $scope.data = {
+      showDelete: false
+    };
+
+    $scope.items = {}
+
+    $scope.$on('$ionicView.afterEnter', function () {
+      console.log("LOADED!")
+    });
+
+    $scope.loadNotes = function () {
+      var token = sessionStorage.getItem("token");
+      var userId = sessionStorage.getItem("userId");
+      $http.defaults.headers.common['x-access-token'] = token;
+      Item.query({userId: userId}, function (data) {
+        $scope.items = data
+      })
+    };
+
+    $scope.showConfirm = function (execute) {
+      var confirmPopup = $ionicPopup.confirm({
+        title: 'Delete account',
+        template: 'Are you sure you want to delete this account?'
+      });
+
+      confirmPopup.then(function (res) {
+        if (res) {
+          execute()
+        } else {
+        }
+      });
+    };
+
+    $scope.deleteItem = function (item) {
+      Item.delete({id: item._id});
+      var index = $scope.items.indexOf(item);
+      $scope.items.splice(index, 1);
+    };
+
+    $scope.toggleSideMenu = function () {
+      $ionicSideMenuDelegate.toggleLeft();
+    };
+
+    $scope.addNewItem = function () {
+      $state.go('item');
+    };
+
+    $scope.logout = function () {
+      $ionicHistory.clearCache();
+      $state.go('login', {}, {reload: true});
+    };
+
+    $scope.deleteAccount = function () {
+      $scope.showConfirm(function () {
+        var token = sessionStorage.getItem("token");
+        var userId = sessionStorage.getItem("userId");
+        $http.defaults.headers.common['x-access-token'] = token;
+        User.delete({id: userId});
+        $state.go('login', {}, {reload: true});
+        Notification.show(response.message);
+      })
+    };
+
+    $scope.reloadNotes = function () {
+      $scope.items = {};
+      $scope.loadNotes();
+      $scope.$broadcast('scroll.refreshComplete');
+    };
+
+    $scope.loadNotes();
+
+
+  })
+
+  .controller('RegisterController', function ($state, $http, $scope, Variables, Notification) {
+
+    $scope.user = {}
+
+    $scope.back = function () {
+      $state.go("login")
+    };
+
+    $scope.register = function (form) {
+      if (form.$valid) {
+        var user = $scope.user;
+        var registerUrl = Variables.serverAddress + "register"
+        $http({
+          method: 'POST',
+          url: registerUrl,
+          data: {email: user.username, password: user.password}
+        }).success(function (response) {
+          $http.defaults.headers.common['x-access-token'] = response.token;
+          sessionStorage.setItem("token", response.token);
+          sessionStorage.setItem("userId", response.userId);
+          Notification.show(response.message);
+          $state.go("itemList")
+        }).error(function (err) {
+          Notification.show(err);
+        });
+      }
+    };
+  })
+
+  .controller('LoginController', function ($state, $http, $scope, Variables, Notification) {
+
+    $scope.user = {};
+    $scope.hideError = true;
+    $scope.loginError = "Placeholder";
+
+    $scope.signIn = function () {
+      var user = $scope.user;
+      var loginUrl = Variables.serverAddress + "login";
+
+      $http({
+        method: 'POST',
+        url: loginUrl,
+        data: {email: user.username, password: user.password}
+      }).success(function (response) {
+        $http.defaults.headers.common['x-access-token'] = response.token;
+        sessionStorage.setItem("token", response.token);
+        sessionStorage.setItem("userId", response.userId);
+        $state.go("itemList")
+      }).error(function (data, status) {
+        $scope.loginError = data.message;
+        $scope.hideError = false;
+      });
+    };
+  });
